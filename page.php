31ad13fcc033dbb8a04ca186671705a5a46bf8b0
xechnax <rsortilla@gmail.com>
2025-02-15 12:54:50 +0800

    Initial template for pages.

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template for displaying default pages.
+ * 
+ */
+
+get_header();
+
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+endwhile;
+
+get_footer();
