a74e1b72af0c298d762d6e0c30a265ec02ee31b1
xechnax <rsortilla@gmail.com>
2025-02-19 19:56:22 +0800

    Updated class names for uniformity.

--- template-parts/header/header-site.php
+++ template-parts/header/header-site.php
@@ -1,4 +1,4 @@
-<div class="is-layout-constrained theme-header-wrapper" itemscope itemtype="https://schema.org/WPHeader">
+<div class="is-layout-constrained agisty-wrapper-header" itemscope itemtype="https://schema.org/WPHeader">
 	<header id="header" class="is-layout-flex alignwide agisty__header">
 		<nav class="wp-block-navigation is-layout-flex header-menu__nav">
 			<?php 
