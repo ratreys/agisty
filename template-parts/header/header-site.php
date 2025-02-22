6a6b11c149305fe7a78af4524794ee1815cbcff2
xechnax <rsortilla@gmail.com>
2025-02-22 19:31:43 +0800

    Simplified classnames.

--- template-parts/header/header-site.php
+++ template-parts/header/header-site.php
@@ -1,6 +1,6 @@
 <div class="is-layout-constrained agisty-wrapper-header" itemscope itemtype="https://schema.org/WPHeader">
 	<header id="header" class="is-layout-flex alignwide agisty__header">
-		<nav class="wp-block-navigation is-layout-flex header-menu__nav">
+		<nav class="is-layout-flex wp-block-navigation header-menu__nav">
 			<?php 
 			get_template_part( 'template-parts/header/header-logo' );
 			get_template_part( 'template-parts/header/header-menu' ); 
