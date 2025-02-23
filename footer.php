a35a6ed7efe3de458d3fe47068f3eaed2dfdcef9
xechnax <rsortilla@gmail.com>
2025-02-23 12:16:45 +0800

    Simplified footer structure.

--- footer.php
+++ footer.php
@@ -16,9 +16,7 @@
 		<div class="footer-menu__container" itemscope itemtype="https://schema.org/SiteNavigationElement">
 			<?php get_template_part( 'template-parts/footer/footer-menu' ); ?>
 		</div>
-		<div class="agisty-footer__copyright">
-			<?php echo '<p>© ' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) . '</p>'; ?>
-		</div>
+		<?php echo '<p>© ' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) . '</p>'; ?>
 	</footer>
 </div>
 <?php wp_footer(); ?>
