a98ef6f6e5ee83d4a8f944c0d146c88430c77654
xechnax <rsortilla@gmail.com>
2025-02-23 12:18:13 +0800

    Simplified classnames for uniformity.

--- template-parts/footer/footer-menu.php
+++ template-parts/footer/footer-menu.php
@@ -5,13 +5,13 @@
 
 if ( has_nav_menu( 'menu-footer' ) ) : 
 ?>
-	<nav id="footer-nav" class="wp-block-navigation is-layout-flex agisty-footer__menu" aria-label="<?php esc_attr_e( 'Footer menu', 'agisty' ); ?>">
+	<nav id="footer-nav" class="is-layout-flex wp-block-navigation agisty-footer__menu" aria-label="<?php esc_attr_e( 'Footer menu', 'agisty' ); ?>">
 		<?php
 		wp_nav_menu( [
 			'container'			=> false,
 			'fallback_cb'		=> false,
 			'items_wrap'		=> '<ul id="footer-menu-list" class="%2$s">%3$s</ul>',
-			'menu_class'		=> 'footer-menu-wrapper is-layout-flex',
+			'menu_class'		=> 'is-layout-flex footer-menu-wrapper',
 			'theme_location'	=> 'menu-footer',
 		] );
 		?>
