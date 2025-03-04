9d7af9648213d4527115e078921195b258d39952
xechnax <rsortilla@gmail.com>
2025-03-05 01:54:45 +0800

    Removed unused variables.

--- single.php
+++ single.php
@@ -11,14 +11,6 @@ while ( have_posts() ) :
 	the_post();
 	get_template_part( 'template-parts/content/content-single' );
 
-	$next_label = esc_html__( 'Next Post', 'agisty' );
-	$prev_label = esc_html__( 'Prev Post', 'agisty' );
-
-	// the_post_navigation( [
-	// 'next-text' => '<p class="meta-nav">' . $next_label . '</p><p class="post-title">%title</p>',
-	// 'prev-text' => '<p class="meta-nav">' . $prev_label . '</p><p class="post-title">%title</p>',
-	// ] );
-
 endwhile;
 
 get_footer();
