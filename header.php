99c4fa2cf4bd5870d2084032025bc5450454edec
xechnax <rsortilla@gmail.com>
2025-03-05 01:01:06 +0800

    Coding Standards: Drop the semicolon for inline statements.

--- header.php
+++ header.php
@@ -1,7 +1,16 @@
+<?php
+/**
+ * Display site header including site branding and navigation.
+ * 
+ * @package Agisty
+ */
+
+?>
+
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes() ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta charset="<?php bloginfo( 'charset' ) ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <!--
 							–▀▀▀▀▀ ▀▀––   ▀▀
@@ -10,20 +19,20 @@
 █    █████        ██ ░▓▓█▀ ░   ▀      █▓▓█ ░     ░█▓▓█ ░
 ▀   █▓▓█        ▄░░─░▀▒▒▒▒▒▒▒█▄──░  █▒▒█ ░ ────   █▒▒█░
 . ┌─█▓▓█        ▀   ░  ▀░░░░░░░█ ░  █░░█ ░        █░░█░
-	: █░░█    ..      ░     .  █..█ ░  █..█ ░      █..█ ░
-	. █..█            ░  ▄▀     █  █ ░ ░█  █▄    ▄█  █  ░
-	| █  █   ──────   ░ ██──────████ ░   ▀▀▄██████▄▀▀
-	└ ▀███            ░ ███▄  ▄████ ░   ░░░░      ░░░░░░░░
+  : █░░█    ..      ░     .  █..█ ░  █..█ ░      █..█ ░
+  . █..█            ░  ▄▀     █  █ ░ ░█  █▄    ▄█  █  ░
+  | █  █   ──────   ░ ██──────████ ░   ▀▀▄██████▄▀▀
+  └ ▀███            ░ ███▄  ▄████ ░   ░░░░      ░░░░░░░░
 		░░░            ░  ▀███████▀          .
 
 	┌──────▀▀▀▀ ▀▀──────── ██ ───────────▀▀▄▄▄  ▄▄▄──────┐
 	: ░                    ▀█▄    ▄             ▀▀       ░ :
 -->
-<?php wp_head(); ?>
+<?php wp_head() ?>
 </head>
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+<body <?php body_class() ?>>
+<?php wp_body_open() ?>
 <a id="skipnav" class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to main content', 'agisty' ); ?></a>
-<?php get_template_part( 'template-parts/header/header-site' ); ?>
+<?php get_template_part( 'template-parts/header/header-site' ) ?>
 <div class="is-layout-constrained agisty-wrapper-main">
 <main id="main" class="is-layout-flex alignwide agisty__main" tabindex="-1">
