68aab473b794b08c8db4a470c88c9a3ec3f6ba7e
xechnax <rsortilla@gmail.com>
2025-03-17 15:07:36 +0800

    Removed `wp-block-post-title` class for entry title.

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -12,9 +12,9 @@
 	<header class="entry-header">
 		<?php
 		if ( is_single() ) :
-			the_title( '<h1 class="wp-block-post-title">', '</h1>' );
+			the_title( '<h1>', '</h1>' );
 		else :
-			the_title( '<h2 class="wp-block-post-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 		?>
 	</header>
