f5d007c2ea322afb5563333a4e10db5ca898c97a
xechnax <rsortilla@gmail.com>
2025-03-19 21:03:17 +0800

    Simplified footer menu template part.

--- template-parts/footer/footer-menu.php
+++ template-parts/footer/footer-menu.php
@@ -7,14 +7,14 @@
 
 if ( has_nav_menu( 'menu-footer' ) ) : 
 	?>
-	<nav id="footer-nav" class="is-layout-flex wp-block-navigation agisty-footer__menu" aria-label="<?php esc_attr_e( 'Footer menu', 'agisty' ) ?>">
+	<nav class="is-layout-flex wp-block-navigation footer-menu__nav" aria-label="<?php esc_attr_e( 'Footer menu', 'agisty' ) ?>">
 		<?php
 		wp_nav_menu( [
-			'container'         => false,
-			'fallback_cb'       => false,
-			'items_wrap'        => '<ul id="footer-menu-list" class="%2$s">%3$s</ul>',
-			'menu_class'        => 'is-layout-flex footer-menu-wrapper',
 			'theme_location'    => 'menu-footer',
+			'menu_class'        => 'is-layout-flex wp-block-navigation__container footer-menu__wrapper',
+			'items_wrap'        => '<ul id="footer-menu-list" class="%2$s">%3$s</ul>',
+			'fallback_cb'       => false,
+			'container'         => false,
 		] );
 		?>
 	</nav>
