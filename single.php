0ec3e552f053ffbfeb14d54a4a70cb6ca24ac450
xechnax <rsortilla@gmail.com>
2025-03-19 21:28:04 +0800

    Coding Standards: Newline required after opening brace.

--- single.php
+++ single.php
@@ -10,7 +10,8 @@ get_header();
 <div class="is-layout-constrained agisty-wrapper__main">
 	<main id="main" class="is-layout-flex alignwide agisty__main" tabindex="-1">
 		<?php
-		while ( have_posts() ) : the_post();
+		while ( have_posts() ) : 
+			the_post();
 			// Display post content.
 			get_template_part( 'template-parts/content/content-single' );
 		endwhile;
