2452448395c8206c9cd5ba55c2e9ee03656e6fc1
xechnax <rsortilla@gmail.com>
2025-03-20 22:36:29 +0800

    Removed unnecessary check for displaying post title.

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -10,13 +10,7 @@
 ?>
 <article <?php post_class() ?>>
 	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1>', '</h1>' );
-		else :
-			the_title( '<h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		?>
+		<?php the_title( '<h1>', '</h1>' ) ?>
 	</header>
 	<div class="entry-content is-layout-flow wp-block-post-content">
 		<?php the_content() ?>
