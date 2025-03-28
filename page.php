4f11d9cb0fe37f517c374be50e29eda232ef944d
xechnax <rsortilla@gmail.com>
2025-03-29 00:29:27 +0800

    Properly display `page` content.

--- page.php
+++ page.php
@@ -6,10 +6,16 @@
  */
 
 get_header();
-
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-endwhile;
-
-get_footer();
+?>
+<div class="is-layout-constrained wrapper ">
+	<main id="main" class="agisty-main__page" tabindex="-1">
+		<?php
+		while ( have_posts() ) : 
+			the_post();
+			// Display post content.
+			get_template_part( 'template-parts/content/content-page' );
+		endwhile;
+		?>
+	</main>
+</div>
+<?php get_footer() ?>
