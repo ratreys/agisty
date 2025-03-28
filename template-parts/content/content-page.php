87f71c491d556abe4b70149b3f2ddf83fac51327
xechnax <rsortilla@gmail.com>
2025-03-29 00:29:57 +0800

    Properly display `page` content.

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -1,3 +1,18 @@
-<article <?php post_class(); ?>>
+<?php
+/**
+ * Template part for displaying all page.
+ * 
+ * @package Agisty
+ */
 
+?>
+<article <?php post_class() ?>>
+	<header class="entry-header">
+		<hgroup class="entry-header__group">
+			<?php the_title( '<h1 class="entry-header__title">', '</h1>' ) ?>
+		</hgroup>
+	</header>
+	<div class="entry-content is-layout-flow">
+		<?php the_content() ?>
+	</div>
 </article>
