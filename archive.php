8b8e7254335e05579c3128ba645e95c4cc586020
xechnax <rsortilla@gmail.com>
2025-03-29 16:57:55 +0800

    Correctly display archive posts for categories and tags.

--- archive.php
+++ archive.php
@@ -6,4 +6,27 @@
  */
 
 get_header();
-get_footer();
+?>
+<div class="width-wide grow">
+<main class="is-layout-constrained agisty-main__archive padding-block__main" id="main" tabindex="-1">
+	<section class="alignwide">
+		<ul class="is-layout-grid entry-card__wrapper" role="list">
+		<?php
+		while ( have_posts() ) :
+			the_post()
+			?>
+			<li class="entry-card">
+				<?php
+				// Display post content.
+				the_title( '<h2 class="entry-card__heading"><a class="entry-card__link" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				the_excerpt();
+				?>
+			</li>
+			<?php 
+		endwhile;
+		?>
+		</ul>
+	</section>
+</main>
+</div>
+<?php get_footer() ?>
