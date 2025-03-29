1ba3ae7e7d6fe2542ddbf5ab8f9348cf9e5923d3
xechnax <rsortilla@gmail.com>
2025-03-29 17:04:16 +0800

    New HTML class for `page` styling.

--- page.php
+++ page.php
@@ -7,8 +7,8 @@
 
 get_header();
 ?>
-<div class="is-layout-constrained wrapper ">
-	<main id="main" class="agisty-main__page" tabindex="-1">
+<div class="width-wide is-layout-constrained grow">
+	<main id="main" class="pb-main agisty-main__page" tabindex="-1">
 		<?php
 		while ( have_posts() ) : 
 			the_post();
