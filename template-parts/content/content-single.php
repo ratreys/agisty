509f337f9f763942bbaaf73c31b78d0468edfaea
xechnax <rsortilla@gmail.com>
2025-03-29 17:13:08 +0800

    Updated HTML class elements for correct styling of single posts..

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -9,13 +9,15 @@
 
 ?>
 <article <?php post_class() ?>>
-	<header class="entry-header">
-		<hgroup class="entry-header__group">
+	<header class="entry-header post-header">
+		<hgroup class="flow-content post-header__group">
 			<?php the_title( '<h1 class="entry-header__title">', '</h1>' ) ?>
-			<?php the_excerpt() ?>
+			<div class="entry-header__excerpt">
+				<?php the_excerpt() ?>
+			</div>
 		</hgroup>
 	</header>
-	<div class="entry-content is-layout-flow">
+	<div class="entry-content is-layout-flow flow-content">
 		<?php the_content() ?>
 	</div>
 </article>
