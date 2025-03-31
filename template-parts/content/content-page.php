c00460fa5a86fb6b8483b63cb80c05be33084c46
xechnax <rsortilla@gmail.com>
2025-03-31 22:30:51 +0800

    Removed `hrgroup` from within header.

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -8,9 +8,7 @@
 ?>
 <article <?php post_class() ?>>
 	<header class="entry-header page-header">
-		<hgroup class="page-header__group">
-			<?php the_title( '<h1 class="page-header__title">', '</h1>' ) ?>
-		</hgroup>
+		<?php the_title( '<h1 class="page-header__title">', '</h1>' ) ?>
 	</header>
 	<div class="entry-content is-layout-flow flow-content">
 		<?php the_content() ?>
