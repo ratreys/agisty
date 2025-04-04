82c08c3d2efa983b05ab2f4b81d1b753f09c0994
xechnax <rsortilla@gmail.com>
2025-04-04 17:02:08 +0800

    Fix: Page title not being formatted correctly.

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -8,7 +8,7 @@
 ?>
 <article <?php post_class() ?>>
 	<header class="entry-header page-header">
-		<?php the_title( '<h1 class="page-header__title">', '</h1>' ) ?>
+		<?php the_title( '<h1 class="entry-header__title">', '</h1>' ) ?>
 	</header>
 	<div class="entry-content is-layout-flow flow-content">
 		<?php the_content() ?>
