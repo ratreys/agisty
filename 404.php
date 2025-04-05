1f425cffbf0d9d94e3eb2be032ce4755dd112c2e
xechnax <rsortilla@gmail.com>
2025-04-05 21:05:44 +0800

    Added 404 page message and styling.

--- 404.php
+++ 404.php
@@ -6,4 +6,21 @@
  */
 
 get_header();
-get_footer();
+?>
+<div class="wrapper-site__main grow">
+<main id="main" class="is-layout-constrained pb-main width-wide agisty-main__404" tabindex="-1">
+	<article class="type-404">
+		<h1 class="entry-header 404-header">Page not found</h1>
+		<div class="entry-content is-layout-flow flow-content">
+			<p>Sorry, but the page you were looking for might have been removed, renamed, or did not exist in the first place.</p>
+			<!-- Home page link: -->
+			<div class="wp-block-buttons is-content-justification-left is-layout-flex">
+				<div class="wp-block-button">
+					<a class="wp-block-button__link wp-element-button" href="/">Home Page</a>
+				</div>
+			</div>
+		</div>
+	</article>
+</main>
+</div>
+<?php get_footer() ?>
