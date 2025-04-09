b337af9f815c7bb55cd0a2abcb0e2a8e410f18d5
xechnax <rsortilla@gmail.com>
2025-04-09 18:14:31 +0800

    Used template part for displaying 404 pages.

--- 404.php
+++ 404.php
@@ -9,18 +9,7 @@ get_header();
 ?>
 <div class="wrapper-site__main grow">
 <main id="main" class="is-layout-constrained pb-main width-wide agisty-main__404" tabindex="-1">
-	<article class="type-404">
-		<h1 class="entry-header 404-header">Page not found</h1>
-		<div class="entry-content is-layout-flow flow-content">
-			<p>Sorry, but the page you were looking for might have been removed, renamed, or did not exist in the first place.</p>
-			<!-- Home page link: -->
-			<div class="wp-block-buttons is-content-justification-left is-layout-flex">
-				<div class="wp-block-button">
-					<a class="wp-block-button__link wp-element-button" href="/">Home Page</a>
-				</div>
-			</div>
-		</div>
-	</article>
+	<?php get_template_part( 'template-parts/content/content-none' ) ?>
 </main>
 </div>
 <?php get_footer() ?>
