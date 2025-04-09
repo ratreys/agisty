5cf11c84cc6fba91bfb54d0e0c2171d5c08cd27a
xechnax <rsortilla@gmail.com>
2025-04-09 18:13:52 +0800

    Added template part for 404 pages.

---
+++ template-parts/content/content-none.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying 404 pages.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * 
+ * @package Agisty
+ */
+
+?>
+<article class="type-404">
+    <h1 class="entry-header 404-header">Page not found</h1>
+    <div class="entry-content is-layout-flow flow-content">
+        <p>Sorry, but the page you were looking for might have been removed, renamed, or did not exist in the first place.</p>
+        <!-- Home page link: -->
+        <div class="wp-block-buttons is-content-justification-left is-layout-flex">
+            <div class="wp-block-button">
+                <a class="wp-block-button__link wp-element-button" href="/">Home Page</a>
+            </div>
+        </div>
+    </div>
+</article>
+
