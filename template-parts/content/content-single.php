d3aba3686378ba85d36aa9c958ab144093bedca6
xechnax <rsortilla@gmail.com>
2025-04-25 22:23:47 +0800

    Accessibility improvements.

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -8,24 +8,24 @@
  */
 
 ?>
-<article <?php post_class() ?>>
+<article <?php post_class() ?> data-column="content">
 	<header class="entry-header post-header">
 		<hgroup class="flow-content post-header__group">
 			<?php the_title( '<h1 class="entry-header__title">', '</h1>' ) ?>
-			<div class="entry-header__excerpt">
+			<section aria-label="article snippet" class="entry-header__excerpt">
 				<?php the_excerpt() ?>
-			</div>
+			</section>
 			<?php
 			printf(
 				'<p class="entry-header__meta">%1$s<time datetime="%2$s">%3$s</time></p>',
 				esc_html__( 'Updated: ', 'agisty' ),
 				esc_attr( get_the_modified_date( 'c' ) ),
-				esc_html( get_the_modified_date() )
+				esc_html( get_the_modified_date( 'M Y' ) )
 			);
 			?>
 		</hgroup>
 	</header>
-	<div class="entry-content is-layout-flow flow-content">
+	<section aria-label="article content" class="entry-content is-layout-flow flow-content">
 		<?php the_content() ?>
-	</div>
+	</section>
 </article>
