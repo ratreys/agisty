56f26694b5295e0acad01d0efc30eb0e63ecb0b7
xechnax <rsortilla@gmail.com>
2025-04-25 22:24:31 +0800

    Added sidebar.

--- single.php
+++ single.php
@@ -8,14 +8,19 @@
 get_header();
 ?>
 <div class="wrapper-site__main grow">
-<main id="main" class="is-layout-constrained pb-main width-wide agisty-main__post" tabindex="-1">
-	<?php
-	while ( have_posts() ) : 
-		the_post();
-		// Display post content.
-		get_template_part( 'template-parts/content/content-single' );
-	endwhile;
-	?>
+<main id="main" class="width-wide agisty-main__post" tabindex="-1">
+	<div class="pb-48px" data-layout="sidebar">
+		<?php
+		while ( have_posts() ) : 
+			the_post();
+			// Display post content.
+			get_template_part( 'template-parts/content/content-single' );
+		endwhile;
+		?>
+		<aside class="post-sidebar" data-column="sidebar">
+			<h2>Related Articles</h2>
+		</aside>
+	</div>
 </main>
 </div>
 <?php get_footer() ?>
