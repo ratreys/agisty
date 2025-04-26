3dac2951638514a0715507ba3a789f28c186a265
xechnax <rsortilla@gmail.com>
2025-04-26 19:42:03 +0800

    Coding Standards: Proper indentation for better code readability.

--- header.php
+++ header.php
@@ -17,12 +17,12 @@
 <?php wp_body_open() ?>
 <a class="visually-hidden skip-link" href="#main"><?php esc_html_e( 'Skip to main content', 'agisty' ) ?></a>
 <div class="wrapper-site__header" itemscope itemtype="https://schema.org/WPHeader">
-<header id="header" class="is-layout-constrained width-wide agisty-header">
-<nav class="is-layout-flex alignwide wp-block-navigation header-menu__nav">
-<?php 
-get_template_part( 'template-parts/header/header-logo' );
-get_template_part( 'template-parts/header/header-menu' );
-?>
-</nav>
-</header>
+	<header id="header" class="is-layout-constrained width-wide agisty-header">
+		<nav class="is-layout-flex alignwide wp-block-navigation header-menu__nav">
+			<?php 
+			get_template_part( 'template-parts/header/header-logo' );
+			get_template_part( 'template-parts/header/header-menu' );
+			?>
+		</nav>
+	</header>
 </div>
