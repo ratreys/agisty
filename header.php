9c2c71a7ba26eb22150ae9937bd64571569d4a68
xechnax <rsortilla@gmail.com>
2025-05-01 08:32:34 +0800

    Site header styling update.

--- header.php
+++ header.php
@@ -16,13 +16,13 @@
 <body <?php body_class() ?>>
 <?php wp_body_open() ?>
 <a class="visually-hidden skip-link" href="#main"><?php esc_html_e( 'Skip to main content', 'agisty' ) ?></a>
-<div class="wrapper-site__header" itemscope itemtype="https://schema.org/WPHeader">
-	<header id="header" class="is-layout-constrained width-wide agisty-header">
-		<nav class="is-layout-flex alignwide wp-block-navigation header-menu__nav">
-			<?php 
-			get_template_part( 'template-parts/header/header-logo' );
-			get_template_part( 'template-parts/header/header-menu' );
-			?>
-		</nav>
-	</header>
+<div class="is-layout-constrained wrapper-site__header" itemscope itemtype="https://schema.org/WPHeader">
+<header id="header" class="alignwide width-wide agisty-header">
+<nav class="is-layout-flex wp-block-navigation header-menu__nav">
+<?php 
+get_template_part( 'template-parts/header/header-logo' );
+get_template_part( 'template-parts/header/header-menu' );
+?>
+</nav>
+</header>
 </div>
