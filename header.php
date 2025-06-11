8d451231edb71f6a86281343115581597477065d
xechnax <rsortilla@gmail.com>
2025-06-11 21:25:37 +0800

    Simplified site header.

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Display site header including site branding and navigation.
- * 
+ *
  * @package Agisty
  */
 
@@ -9,20 +9,20 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ) ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<?php wp_head() ?>
+    <meta charset="<?php bloginfo( 'charset' ) ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
-<?php wp_body_open() ?>
-<a class="visually-hidden skip-link" href="#main"><?php esc_html_e( 'Skip to main content', 'agisty' ) ?></a>
-<div class="is-layout-constrained wrapper-site__header" itemscope itemtype="https://schema.org/WPHeader">
-<header id="header" class="alignwide width-wide agisty-header">
-<nav class="is-layout-flex wp-block-navigation header-menu__nav">
-<?php 
-get_template_part( 'template-parts/header/header-logo' );
-get_template_part( 'template-parts/header/header-menu' );
-?>
-</nav>
-</header>
-</div>
+    <?php wp_body_open() ?>
+    <a class="screen-reader-text skip-link" href="#main"><?php esc_html_e( 'Skip to main content', 'agisty' ) ?></a>
+    <div class="wrapper-site__head" itemscope itemtype="https://schema.org/WPHeader">
+        <header id="header" class="is-layout-constrained agisty-header">
+            <nav class="alignwide wp-block-navigation">
+                <?php
+                get_template_part( 'template-parts/header/header-logo' );
+                get_template_part( 'template-parts/header/header-menu' );
+                ?>
+            </nav>
+        </header>
+    </div>
