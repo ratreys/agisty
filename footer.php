943c61286a549d79b42f4d18751acc9c0011c53c
xechnax <rsortilla@gmail.com>
2025-06-12 10:40:49 +0800

    Coding Standards: Proper spacing and indentation.

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * The template for displaying the footer.
- * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
@@ -9,22 +8,22 @@
  */
 
 ?>
-<div class="wrapper-site__footer" itemscope itemtype="https://schema.org/WPFooter">
-    <footer id="footer" class="agisty-footer">
-        <div class="wrapper-nav__footer" itemscope itemtype="https://schema.org/SiteNavigationElement">
-            <?php get_template_part( 'template-parts/footer/footer-menu' ) ?>
-        </div>
-        <div class="wrapper-agisty__copyright">
-            <?php
-            printf(
-                '<p class="agisty-copyright width-wide">© <time datetime="%1$s">%2$s</time> %3$s</p>',
-                esc_attr( gmdate( 'Y' ) ),
-                esc_html( gmdate( 'Y' ) ),
-                esc_html( get_bloginfo( 'name' ) )
-            );
-            ?>
-        </div>
-    </footer>
+<div class="wrapper-site__foot" itemscope itemtype="https://schema.org/WPFooter">
+<footer id="footer" class="is-layout-constrained agisty-footer">
+	<div class="alignwide wrapper-nav__footer" itemscope itemtype="https://schema.org/SiteNavigationElement">
+		<?php get_template_part( 'template-parts/footer/footer-menu' ) ?>
+	</div>
+	<div class="alignwide wrapper-agisty__copyright">
+		<?php
+		printf(
+			'<p class="agisty-copyright">© <time datetime="%1$s">%2$s</time> %3$s</p>',
+			esc_attr( gmdate( 'Y' ) ),
+			esc_html( gmdate( 'Y' ) ),
+			esc_html( get_bloginfo( 'name' ) )
+		);
+		?>
+	</div>
+</footer>
 </div>
 <?php wp_footer() ?>
 </body>
